8c6267d042d4c47bfeeebcdbad1be17721530435
Matheus Olegario <183168566+omatheusolegario@users.noreply.github.com>
2026-09-17 11:54:08 -0300

    Corrigindo alguns erros/ adicionando tratamento correto de erros na parte de logs.

--- lib/build.sh
+++ lib/build.sh
@@ -1,7 +1,6 @@
 build_project() {
-
   #Confere se o gcc existe no sistema
-  debug_msg "DEBUG: Conferindo se o gcc existe no sistema..."
+  debug_msg "Conferindo se o gcc existe no sistema..."
   if command -v gcc >/dev/null 2>&1; then
     verbose_msg "GCC encontrado."
   else
@@ -10,7 +9,7 @@ build_project() {
   fi
 
   #Confere se o make existe no sistema
-  debug_msg "DEBUG: Conferindo se o make existe no sistema..."
+  debug_msg "Conferindo se o make existe no sistema..."
   if command -v make >/dev/null 2>&1; then
     verbose_msg "Make encontrado."
   else
@@ -18,13 +17,13 @@ build_project() {
     return 1
   fi
 
-  debug_msg "DEBUG: Conferindo se existem arquivos fonte..."
+  debug_msg "Conferindo se existem arquivos fonte..."
   #Confere se existem arquivos fonte
   if [[ ${#SOURCE_FILES[@]} -eq 0 ]]; then
     echo "Não há arquivos fonte especificados" >&2
     return 1
   fi
-  debug_msg "DEBUG: Existem arquivos fonte."
+  debug_msg "Existem arquivos fonte."
 
   #Array para armazenar os arquivos objeto
   local objetos=()
@@ -32,7 +31,7 @@ build_project() {
   #Verificação de mudança de flag de otimização
   local forcar_recompilacao=0
 
-  debug_msg "DEBUG: Conferindo se o nível de otimização mudou..."
+  debug_msg "Conferindo se o nível de otimização mudou..."
 
   if [[ ! -f "$BUILD_DIR/OPT_LEVEL.txt" || "$(<"$BUILD_DIR/OPT_LEVEL.txt")" != "$OPT_LEVEL" ]]; then
     verbose_msg "O nível de otimização mudou. Forçando recompilação de todos arquivos."
@@ -42,16 +41,16 @@ build_project() {
       return 1
     fi
   else
-    debug_msg "DEBUG: O nível de otimização não mudou."
+    debug_msg "O nível de otimização não mudou."
   fi
 
   #Itera cada arquivo fonte, e verifica se é necessária uma compilação dele
   local arquivo_fonte
 
-  debug_msg "DEBUG: Entrando na iteração de arquivos fonte..."
+  debug_msg "Entrando na iteração de arquivos fonte..."
   for arquivo_fonte in "${SOURCE_FILES[@]}"; do
 
-    debug_msg "DEBUG: Arquivo fonte atual: $arquivo_fonte"
+    debug_msg "Arquivo fonte atual: $arquivo_fonte"
 
     #Atribuição de nomes únicos ao objeto e dependência em build
     local caminho_relativo="${arquivo_fonte#"$PROJECT_DIR/"}"
@@ -59,36 +58,37 @@ build_project() {
     local caminho_obj="$BUILD_DIR/${nome_unico}.o"
     local caminho_dep="$BUILD_DIR/${nome_unico}.d"
 
-    debug_msg "DEBUG: caminho_relativo: $caminho_relativo"
-    debug_msg "DEBUG: nome_unico: $nome_unico"
-    debug_msg "DEBUG: caminho_obj: $caminho_obj"
-    debug_msg "DEBUG: caminho_dep: $caminho_dep"
+    debug_msg "caminho_relativo: $caminho_relativo"
+    debug_msg "nome_unico: $nome_unico"
+    debug_msg "caminho_obj: $caminho_obj"
+    debug_msg "caminho_dep: $caminho_dep"
 
     #Adiciona a lista de objetos o caminho do objeto do arquivo fonte atual
     objetos+=("$caminho_obj")
-    debug_msg "DEBUG: objetos: ${objetos[*]}"
+    debug_msg "objetos: ${objetos[*]}"
 
-    debug_msg "DEBUG: Criando diretório do objeto em $caminho_obj..."
+    debug_msg "Criando diretório do objeto em $caminho_obj..."
     #Cria o diretório onde o objeto vai ficar dentro de $BUILD_DIR, pois preservamos as "/" no nome único
     if ! mkdir -p "$(dirname "$caminho_obj")"; then
       echo "Erro: Não foi possível criar o diretório do objeto." >&2
       return 1
     fi
-    debug_msg "DEBUG: Diretório do objeto criado."
+    debug_msg "Diretório do objeto criado."
 
     #Variável binária (0 ou 1), que decide se é necessário compilar, iniciada com o valor de forçar recompilação que também é binária (0 ou 1)
     local precisa_compilar="$forcar_recompilacao"
-    debug_msg "DEBUG: precisa_compilar: $precisa_compilar"
+    debug_msg "precisa_compilar: $precisa_compilar"
 
-    debug_msg "DEBUG: Checando se não existe o objeto ou a dependência..."
+    debug_msg "Checando se não existe o objeto ou a dependência..."
     #Checagem para ver se não existe ou o objeto ou a dependência
     if [[ ! -f "$caminho_obj" || ! -f "$caminho_dep" ]]; then
       precisa_compilar=1
+      debug_msg "precisa_compilar: $precisa_compilar"
     else
-      debug_msg "DEBUG: Existem ambos."
+      debug_msg "Existem ambos."
     fi
 
-    debug_msg "DEBUG: Checando se ou o .c ou suas dependências foram atualizadas..."
+    debug_msg "Checando se ou o .c ou suas dependências foram atualizadas..."
 
     local arquivo_fonte_formatado_make="${arquivo_fonte// /\\ }"
     local caminho_obj_formatado_make="${caminho_obj// /\\ }"
@@ -105,7 +105,7 @@ $caminho_obj_formatado_make: $arquivo_fonte_formatado_make ; @:
 include $caminho_dep_formatado_make
 EOF
 
-      debug_msg "DEBUG: status_make: $status_make"
+      debug_msg "status_make: $status_make"
       case $status_make in
       0) precisa_compilar=0 ;;
       1) precisa_compilar=1 ;;
@@ -123,7 +123,7 @@ EOF
       # -c para compilar até o arquivo de objeto, -I para especificar onde o compilador pode buscar por cabeçalhos,
       # -MMD para gerar um arquivo de dependências locais, -MP para o make não falhar com dependências antigas,
       # -O para o nível de otimização e -o para especificar o caminho do objeto gerado
-      gcc -c -I "$PROJECT_DIR/include" -MMD -MP "$arquivo_fonte" -O"$OPT_LEVEL" -o "$caminho_obj"
+      gcc -c -I "$PROJECT_DIR/include" -MMD -MP "$arquivo_fonte" -O"$OPT_LEVEL" -o "$caminho_obj" 2>/dev/null
 
       if [[ $? -ne 0 ]]; then
         echo "Erro: Não foi possível compilar o arquivo fonte" >&2
@@ -134,7 +134,7 @@ EOF
       verbose_msg "O arquivo objeto $caminho_obj está atualizado"
     fi
   done
-  debug_msg "DEBUG: Finalizada a iteração de arquivos fonte."
+  debug_msg "Finalizada a iteração de arquivos fonte."
 
   #Depois de conferir se cada arquivo fonte está atualizado, agora é feito o ligamento entre os objetos
   verbose_msg "Ligando arquivos objeto..."
@@ -148,7 +148,8 @@ EOF
     echo "Erro: Não foi possível salvar o nível de otimização" >&2
     return 1
   else
-    echo "Build concluída com sucesso. O executável está disponível em $EXECUTABLE"
+    echo "Build concluída com sucesso."
+    echo "O executável está disponível em: $EXECUTABLE"
     return 0
   fi
 }
